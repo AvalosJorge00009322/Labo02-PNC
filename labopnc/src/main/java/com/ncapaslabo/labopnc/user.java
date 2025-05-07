7e48513d12b9652c169f23944eaf82857dbe33d3
AvalosJorge00009322 <00009322@uca.edu.sv>
2025-05-07 16:19:37 -0600

    load files

---
+++ labopnc/src/main/java/com/ncapaslabo/labopnc/user.java
@@ -0,0 +1,11 @@
+package com.ncapaslabo.labopnc;
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Table
+public class user {
+}
