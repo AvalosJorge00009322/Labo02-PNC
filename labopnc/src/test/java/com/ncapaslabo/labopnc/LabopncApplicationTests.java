2f66cb5c205ad70fc5332bb7829de3d76b3b43fc
AvalosJorge00009322 <00009322@uca.edu.sv>
2025-05-06 23:45:58 -0600

    first commit

---
+++ labopnc/src/test/java/com/ncapaslabo/labopnc/LabopncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncapaslabo.labopnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabopncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
