dd67c5c0258cde6ea6f22a071779ef5970423b64
AvalosJorge00009322 <00009322@uca.edu.sv>
2025-05-07 19:46:27 -0600

    adding examples

---
+++ labopnc/src/main/java/com/ncapaslabo/labopnc/Repositories/iGenericRepository.java
@@ -0,0 +1,12 @@
+package com.ncapaslabo.labopnc.Repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface iGenericRepository <T,ID> extends JpaRepository<T,ID> {
+
+
+}
